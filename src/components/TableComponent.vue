4233b232be0ca2109b39eda5e22beb88a86108be
Fílip Anselmo <filip.anselmo@jointecnologia.com.br>
2022-11-23 16:26:46 -0400

    Table items Ok

--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -4,18 +4,28 @@
     :items="tableItems"
     :items-per-page="5"
     class="elevation-1"
-  ></v-data-table>
+  >
+    <template v-slot:body="{ items }">
+      <tbody>
+        <tr v-for="(item, index) in items" :key="index">
+          <td>
+            <span>{{item.name}}</span>
+          </td>
+          <td>
+            <span>{{item.duration}}</span>
+          </td>
+        </tr>
+      </tbody>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
 export default {
   name: "TableComponent",
-  props:['tableItems'],
+  props: ["tableItems"],
   data: () => ({
-    headers: [
-      {text: 'Name'},
-      {text: 'Duration'},
-    ],
+    headers: [{ text: "Name" }, { text: "Duration" }],
     // localItems:[],
   }),
   // watch:{
