0286e1abc7adebf7f7c65142c37d74ccb7833059
Fílip Anselmo <filip.anselmo@jointecnologia.com.br>
2022-11-26 07:51:40 -0400

    xs 6

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -2,46 +2,46 @@
   <div class="text-center">
     <div class="mt-3">
         <v-row>
-            <v-col cols="4">
+            <v-col cols="4" xs="6">
                 <slipknot-component></slipknot-component>
             </v-col>
-            <v-col cols="4">
+            <v-col cols="4" xs="6">
                 <korn-component></korn-component>
             </v-col>
-            <v-col cols="4">
+            <v-col cols="4" xs="6">
                 <avril-lavigne-component></avril-lavigne-component>
             </v-col>
         </v-row>
          <v-row>
-            <v-col cols="4">
+            <v-col cols="4" xs="6">
                 <limp-bizkit-component></limp-bizkit-component>
             </v-col>
-            <v-col cols="4">
+            <v-col cols="4" xs="6">
                 <korn-component></korn-component>
             </v-col>
-            <v-col cols="4">
-                <korn-component></korn-component>
-            </v-col>
-        </v-row>
-         <v-row>
-            <v-col cols="4">
-                <slipknot-component></slipknot-component>
-            </v-col>
-            <v-col cols="4">
-                <korn-component></korn-component>
-            </v-col>
-            <v-col cols="4">
+            <v-col cols="4" xs="6">
                 <korn-component></korn-component>
             </v-col>
         </v-row>
          <v-row>
-            <v-col cols="4">
+            <v-col cols="4" xs="6">
                 <slipknot-component></slipknot-component>
             </v-col>
-            <v-col cols="4">
+            <v-col cols="4" xs="6">
                 <korn-component></korn-component>
             </v-col>
-            <v-col cols="4">
+            <v-col cols="4" xs="6">
+                <korn-component></korn-component>
+            </v-col>
+        </v-row>
+         <v-row>
+            <v-col cols="4" xs="6">
+                <slipknot-component></slipknot-component>
+            </v-col>
+            <v-col cols="4" xs="6">
+                <korn-component></korn-component>
+            </v-col>
+            <v-col cols="4" xs="6">
                 <korn-component></korn-component>
             </v-col>
         </v-row>
